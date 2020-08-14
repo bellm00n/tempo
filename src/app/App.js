e570f7598f8c53350de31220aef86944d54f1cd1
bellm00n <bellinthegame@gmail.com>
2020-08-14 21:04:50 +0300

    Refactor App.js

--- src/app/App.js
+++ src/app/App.js
@@ -8,21 +8,16 @@ import 'theme/theme.css'
 import 'theme/fonts.css'
 import 'theme/globalStyle.css'
 
-
 const App = () => (
-  <Router>
-    <Link to="/">Home</Link>
-    <br />
-    <Link to="/project">Project</Link>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/project" exact component={Projects} />
-      {/* <Route path="/login/" exact component={Login} />
-    <Route path="/reg/" exact component={Reg} />
-    <Route path="/project/:name" exact component={Project} />
-    <Route component={Page404} /> */}
-    </Switch>
-  </Router>
+    <div className="app">
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/project" exact component={Projects}/>
+                <Route path="/signup/" exact component={SignUp}/>
+            </Switch>
+        </Router>
+    </div>
 );
 
 export default App;
