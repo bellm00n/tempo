9bd25bcdc117b94910ba257e18acdb6dd5e464cb
bellm00n <bellinthegame@gmail.com>
2020-08-14 21:54:51 +0300

    Add auth layout

---
+++ src/uikit/Auth/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logoImage from "assets/images/logo.svg"
+import "uikit/Auth/auth.css"
+
+const Auth = ({children, title, type}) => {
+    return <div className="auth dark">
+        <div className="auth__box">
+            <div className="auth__header">
+                <div className="auth__logo">
+                    <img src={logoImage} alt="logo" className="auth__logo-image"/>
+                </div>
+                <h1 className="auth__title">{title}</h1>
+                <h1 className="auth__type">{type}</h1>
+            </div>
+            <div className="auth__body">
+                {children}
+            </div>
+        </div>
+    </div>;
+};
+
+export default Auth;
